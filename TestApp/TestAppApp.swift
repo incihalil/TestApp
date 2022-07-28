b1af0d49950a80d3e6b1cda3142a6612490c2a2d
incihalil <roxdeveloper.halil@gmail.com>
2022-07-28 14:33:06 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Halil İnci on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
